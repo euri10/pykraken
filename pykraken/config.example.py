33f9808ec70ed70215873ff2636e778e64fbb753
euri10 <benoit.barthelet@gmail.com>
2016-05-30 09:52:52 +0200

    use env variable for api keys

--- pykraken/config.example.py
+++
@@ -1,5 +0,0 @@
-# in case like me you work under a firewall, if not set PROXY=None
-PROXY = {'proxies': {"https": "https://user:pass@proxy.adress:proxyport"}}
-# create a key at https://www.kraken.com/u/settings/api
-API_KEY = 'Key required to make queries to the API.'
-PRIVATE_KEY = 'Private key used to sign API messages.'
---
+++ pykraken/config.py
@@ -0,0 +1,5 @@
+import os
+
+PROXY = None
+API_KEY = os.environ['K_API_KEY']
+PRIVATE_KEY = os.environ['K_PRIVATE_KEY']
