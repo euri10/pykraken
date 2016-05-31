2593cb0380f461d6167fd58b8e18add9a0c13162
euri10 <benoit.barthelet@gmail.com>
2016-05-31 10:07:22 +0200

    version for fucking readthefuckingdocs

--- pykraken/__init__.py
+++ pykraken/__init__.py
@@ -1,5 +1,6 @@
 import pkg_resources
-__version__ = pkg_resources.get_distribution("pykraken").version
+#__version__ = pkg_resources.get_distribution("pykraken").version
+__version__ = "0.1.0"
 from .client import Client # NOQA
 
 
